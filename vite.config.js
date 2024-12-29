990e047711980da949c25b032928639116592511
chararch <chararch@qq.com>
2024-12-29 16:16:24 +0800

    add ga

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,10 @@ export default defineConfig({
         const routes = ['daily-quote', 'install']
         const template = fs.readFileSync('./dist/index.html', 'utf-8')
         
-        // 为每个路由生成HTML文件
+        // 为每个路由生成HTML文件，保留 Google Analytics 代码
         routes.forEach(route => {
           const filePath = `./dist/${route}/index.html`
-          // 确保目录存在
           fs.mkdirSync(`./dist/${route}`, { recursive: true })
-          // 写入文件
           fs.writeFileSync(filePath, template)
         })
       }
